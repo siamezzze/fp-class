18acff5deeba78eea66ade4b39427d3d21e3b41f
siamezzze <siamezzze@gmail.com>
2014-10-27 00:00:25 +0300

    Revert "First done"
    This reverts commit 8c262164f0e36bd43a21c315291c9dcced9711d7.

--- class-07/01-genintfile.hs
+++ class-07/01-genintfile.hs
@@ -7,31 +7,4 @@
   4) количество строк в файле.
 -}
 
-import System.Random
-import System.Environment
-
-rand_int :: StdGen -> (Int, StdGen)
-rand_int gen = randomR (2, 20) gen :: (Int, StdGen)
-
-
-
-rand_ints :: ([Char], (Int, (Int, (Int, StdGen)))) -> ([Char], (Int, (Int, (Int, StdGen))))
-rand_ints (_, (minv, (maxv, (nmbints, gen)))) = 
-	let line = unwords $ map show $ take nmbints $ (randomRs (minv, maxv) gen :: [Int] )
-	    newGen = snd (random gen :: (Int, StdGen)) in
-	(line, (minv, (maxv, (nmbints, newGen))))
-
-rand_file :: FilePath -> Int -> Int -> Int -> Int -> IO ()
-rand_file fname minvalue maxvalue nmbints nmblines = do
-  gen <- newStdGen
-  let strr = tail $ take (nmblines + 1) $ map (fst) $ iterate (rand_ints) ([], (minvalue, (maxvalue, (nmbints, gen))))
-  writeFile fname (unlines strr)
-
-main = do 
-  args <- getArgs
-  process args
-
-process ([fname,minvalue,maxvalue,nmbints,nmblines]) = rand_file fname (read minvalue) (read maxvalue) (read nmbints) (read nmblines)
-process _ = do
-  putStrLn "Использование:"
-  putStrLn "<имя файла> <минимальное значение> <максимальное значение> <чисел в строке> <строк в файле>"
+main = undefined
