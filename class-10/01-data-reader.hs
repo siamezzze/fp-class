0b52ae5a1f2d1c4fb96e7309e3fecff845a1afe6
siamezzze <siamezzze@gmail.com>
2014-11-18 06:04:54 +0300

    First done

--- class-10/01-data-reader.hs
+++ class-10/01-data-reader.hs
@@ -9,4 +9,40 @@
    блока do не допускается).
 -}
 
-main = undefined
+import System.Environment
+import Data.List
+import Control.Monad
+
+data Student = Student {
+  name :: String,
+  age :: String,
+  group :: String } deriving (Show, Read, Eq)
+
+readStudent :: String -> Student
+readStudent st = Student name age group where
+  [name, age, group] = words st
+
+readStudents :: String -> [Student]
+readStudents = map (readStudent) . lines
+
+studentsFromFile :: FilePath -> IO [Student]
+studentsFromFile fname = (readFile fname) >>= return . readStudents
+
+showStudent :: Student -> String
+showStudent (Student name age group) = unwords [name, age, group]
+
+showStudents :: [Student] -> String
+showStudents = unlines . map showStudent
+
+instance Ord Student where
+  compare (Student n1 a1 g1) (Student n2 a2 g2) = compare n1 n2
+
+studentsToFile :: FilePath -> [Student] -> IO()
+studentsToFile fname students = return (showStudents students) >>= writeFile fname
+
+combineStLists :: [Student] -> [Student] -> [Student]
+combineStLists l1 l2 = sort (l1 ++ l2)
+
+process [fname1, fname2, fnameout] = (liftM2 combineStLists (studentsFromFile fname1) (studentsFromFile fname2)) >>= (studentsToFile fnameout)
+
+main = getArgs >>= process
